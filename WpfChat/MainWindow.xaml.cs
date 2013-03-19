84b0c7726241f7d0e84f06e6452929d1bc76b14c
Kristian Walle <kristian.walle@gmail.com>
2013-03-19 22:25:24 +0100

    Added WPF chat client

---
+++ WpfChat/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Threading;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace WpfChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private async void Start()
+        {
+            var hubConnection = new HubConnection("http://localhost:56031/");
+            var chatHubProxy = hubConnection.CreateHubProxy("chatHub");
+
+            chatHubProxy.On<string, string>("sendMessage",
+                                            (userName, message) =>
+                                            Dispatcher.Invoke(DispatcherPriority.Normal,
+                                                              new Action<string, string>(SendMessage), userName, message));
+
+            await hubConnection.Start();
+
+            BtnSend.Click += (sender, args) =>
+                {
+                    chatHubProxy.Invoke("broadcastMessage", "WPF", Message.Text);
+                    Message.Text = string.Empty;
+                };
+        }
+
+        private void SendMessage(string userName, string message)
+        {
+            Messages.Text += Environment.NewLine + userName + " sier: " + message;
+        }
+    }
+}
