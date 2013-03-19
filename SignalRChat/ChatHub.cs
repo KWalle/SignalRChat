808dd86648a380ebc55eb4021847b7577ea4aa17
Kristian Walle <kristian.walle@gmail.com>
2013-03-19 20:48:50 +0100

    Installed signalr. Enabled package restore. Added hub.

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void BroadcastMessage(string userName, string message)
+        {
+            Clients.All.sendMessage(userName, message);
+        }
+    }
+}
