6aeafe2a89f689392a0c789e421f0ae1d9469681
Kristian Walle <kristian.walle@gmail.com>
2013-03-19 21:13:21 +0100

    Added html chat-page and javascript

---
+++ SignalRChat/Scripts/ChatPage.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="jquery-1.6.4.js" />
+/// <reference path="jquery.signalR-1.0.1.js" />
+$(function() {
+    $('#HiddenUserName').val(prompt('Ditt brukernavn: ', ''));  //Get username..not the best way but works..
+
+    var chatHubProxy = $.connection.chatHub;
+
+    chatHubProxy.client.sendMessage = function(usr, msg) {
+        $('#Messages').append('<li>' + usr + ' sier: ' + msg + '</li>');
+    };
+
+    $.connection.hub.start().done(function () {
+        var usrName = $('#HiddenUserName').val();
+        $('#BtnSendMessage').click(function() {
+            var msgToSend = $('#Message').val();
+            chatHubProxy.server.broadcastMessage(usrName, msgToSend);
+            $('#Message').val('');
+        });
+    });
+});
